e124791992107e769c14d7168b021ab1d24d1f14
Tom Wassenberg <mail@tomwassenberg.nl>
2017-08-20 00:14:07 +0200

    Get rid of parameters

--- fetch-ocsp-responses.sh
+++ fetch-ocsp-responses.sh
@@ -4,28 +4,24 @@
 ## This script fetches OCSP responses, to be used by nginx, utilizing the OCSP
 ## URL embedded in a certificate. This primes the OCSP cache of nginx, because
 ## the OCSP responses get saved in locations that can be referenced in the nginx
-## configurations of the websites that use the certificates. It can be used in
-## two modes.
+## configurations of the websites that use the certificates. The script can
+## behave in two ways.
 ##
-## When this script is called by Certbot as a deploy hook, Certbot passes the
-## "--certbot-deploy-hook" flag. In this case only the OCSP response for the
-## specific website whose certificate is (re)issued by Certbot, is fetched.
+## When this script is called by Certbot as a deploy hook, this is recognized by
+## checking if the variables are set that Certbot passes to its deploy hooks. In
+## this case only the OCSP response for the specific website whose certificate
+## is (re)issued by Certbot, is fetched.
 ##
-## When "--all" is passed, the script cycles through all sites that have a
-## certificate directory in Certbot's folder, and fetches an OCSP response.
+## When Certbot's variables are not passed, the script cycles through all sites
+## that have a certificate directory in Certbot's folder, and fetches an OCSP
+## response.
 ##
-## USAGE: fetch-ocsp-responses.sh [--certbot-deploy-hook] [--all]
+## USAGE: fetch-ocsp-responses.sh
 ##
 
 # Unofficial Bash strict mode
 set -eEfuo pipefail
 IFS=$'\n\t'
-
-USAGE='USAGE: fetch-ocsp-responses.sh [--certbot-deploy-hook] [--all]'
-
-print_to_stderr() {
-  echo 1>&2 "${1}"
-}
 
 # Generates file used by ssl_stapling_file in nginx config of websites
 fetch_ocsp_response() {
@@ -52,45 +48,30 @@ if [[ "${EUID}" != "0" ]]; then
   exit 1
 fi
 
-if [[ -z ${1+x} ]]; then
-  print_to_stderr "${USAGE}"
-  exit 1
-fi
+if [[ -z ${RENEWED_DOMAINS+x} || -z ${RENEWED_LINEAGE+x} ]]; then
+  {
+    FETCH_ALL="1"
+    CERT_DIRECTORY="/etc/letsencrypt/live"
 
-case "${1}" in
-  --certbot-deploy-hook)
-    {
-      if [[ -z ${RENEWED_DOMAINS+x} || -z ${RENEWED_LINEAGE+x} ]]; then
-        print_to_stderr "ERROR: This parameter can only be used from Certbot, for when it runs this script as a deploy hook."
-        exit 1
-      fi
-
-      WEBSITE="$(echo "${RENEWED_DOMAINS}" | awk '{print $1}')"
-      CERT_SUBDIRECTORY="${RENEWED_LINEAGE}"
+    for WEBSITE in $(find ${CERT_DIRECTORY} -type d | grep -oP \
+    '(?<=/live/).+$')
+    do
+      CERT_SUBDIRECTORY="${CERT_DIRECTORY}/${WEBSITE}"
 
       fetch_ocsp_response
-    } 1> /dev/null
-    ;;
-  --all)
-    {
-      CERT_DIRECTORY="/etc/letsencrypt/live"
+    done
+  } 1> /dev/null
+else
+  {
+    WEBSITE="$(echo "${RENEWED_DOMAINS}" | awk '{print $1}')"
+    CERT_SUBDIRECTORY="${RENEWED_LINEAGE}"
 
-      for WEBSITE in $(find ${CERT_DIRECTORY} -type d | grep -oP \
-      '(?<=/live/).+$')
-      do
-        CERT_SUBDIRECTORY="${CERT_DIRECTORY}/${WEBSITE}"
-
-        fetch_ocsp_response
-      done
-    } 1> /dev/null
-    ;;
-  *)
-    print_to_stderr "${USAGE}"
-    exit 1
-esac
+    fetch_ocsp_response
+  } 1> /dev/null
+fi
 
 /usr/sbin/service nginx reload 1> /dev/null
 
-if [[ "${1}" == "--all" ]]; then
-  echo "Fetching of OCSP responses successful! nginx is reloaded to cache the new responses."
+if [[ "${FETCH_ALL}" == "1" ]]; then
+  echo "Fetching of OCSP response(s) successful! nginx is reloaded to cache any new responses."
 fi
