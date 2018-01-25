tomwassenberg/certbot-ocsp-fetcher

commit d425f422be1d85e29a4b3bcf58783baa252cac0a

Change scope of variable
And split declaration and assignment of other variable (SC2155)

certbot-ocsp-fetcher.sh
@@ -14,7 +14,8 @@ parse_cli_arguments() {
           declare -gr OUTPUT_DIR="${1}"; shift
           ;;
         -c|--certbot-dir)
-          declare -gr CERTBOT_DIR="$(readlink -fn -- "${1}")"; shift
+          declare -gr CERTBOT_DIR
+          CERTBOT_DIR="$(readlink -fn -- "${1}")"; shift
           ;;
         *)
           echo \
@@ -71,7 +72,7 @@ fetch_ocsp_response() {
   if [[ "${FETCH_ALL}" == "true" ]]; then
     local -r CERT_DIR="${CERTBOT_DIR}/live/${CERT_NAME}"
   else
-    CERT_DIR="${RENEWED_LINEAGE}"
+    local -r CERT_DIR="${RENEWED_LINEAGE}"
   fi
 
   # Enforce that the OCSP URL is always plain HTTP, because HTTPS URL's are not

commit e7ee13998c15081b1afd778919656d3302247f31

Fix parameter bug introduced in d425f42
Couldn't be set any more, because it was already read-only, so changed order.

Also changed "declare -gr" everywhere to the more readable "readonly".

certbot-ocsp-fetcher.sh
@@ -11,11 +11,11 @@ parse_cli_arguments() {
 
       case ${PARAMETER} in
         -o|--output-dir)
-          declare -gr OUTPUT_DIR="${1}"; shift
+          readonly OUTPUT_DIR="${1}"; shift
           ;;
         -c|--certbot-dir)
-          declare -gr CERTBOT_DIR
           CERTBOT_DIR="$(readlink -fn -- "${1}")"; shift
+          readonly CERTBOT_DIR
           ;;
         *)
           echo \
@@ -29,17 +29,17 @@ parse_cli_arguments() {
 
 process_website_list() {
   if [[ -z ${OUTPUT_DIR+x} ]]; then
-    declare -gr OUTPUT_DIR="/etc/nginx/ocsp-cache"
+    readonly OUTPUT_DIR="/etc/nginx/ocsp-cache"
   fi
   mkdir -p -- ${OUTPUT_DIR}
 
   # These two environment variables are set if this script is invoked by Certbot
   if [[ -z ${RENEWED_DOMAINS+x} || -z ${RENEWED_LINEAGE+x} ]]; then
       # Run in "check every certificate" mode
-      declare -gr FETCH_ALL="true"
+      readonly FETCH_ALL="true"
 
       if [[ -z ${CERTBOT_DIR+x} ]]; then
-        declare -r CERTBOT_DIR="/etc/letsencrypt"
+        readonly CERTBOT_DIR="/etc/letsencrypt"
       fi
 
       local -r LINEAGES=$(find "${CERTBOT_DIR}/live" -type d | \
@@ -51,7 +51,7 @@ process_website_list() {
       unset CERT_NAME
   else
       # Run in Certbot mode, only checking the passed certificate
-      declare -gr FETCH_ALL="false"
+      readonly FETCH_ALL="false"
 
       if [[ -n ${CERTBOT_DIR+x} ]]; then
         echo "The -c/--certbot-dir parameter is not applicable when Certbot is"\