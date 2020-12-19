4d75add71621d76c3790dbc73c7acefce89870d6
Tom Wassenberg <github@tomwassenberg.nl>
2020-12-19 18:21:13 +0100

    Ignore unstaged changes while running hooks/CI
    Also required refactoring CI, so that the actual running of
    linting/formatting/testing could be deduplicated between CI and the pre-commit
    hook.

---
+++ hooks.bash
@@ -0,0 +1,19 @@
+(
+  shopt -s globstar nullglob
+
+  shellcheck --enable all -- certbot-ocsp-fetcher ./**/*.{ba,}sh
+
+  # We need to exclude SC2154 because of
+  # https://github.com/koalaman/shellcheck/issues/1823
+  shellcheck --enable all --exclude 2154 -- ./**/*.bats
+)
+
+shfmt -d -s .
+
+# On CI we use Ubuntu 18.04, whose Bats version doesn't support parallelism
+# yet.
+if [[ ${CI:-} == true ]]; then
+  bats --pretty ./tests
+else
+  bats --pretty --jobs 8 ./tests
+fi
