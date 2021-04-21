9723f1f36184c078b1f66eb18b85fb2cd8113e4c
Tom Wassenberg <github@tomwassenberg.nl>
2021-04-21 19:53:43 +0200

    Fix errexit in tests

--- hooks.bash
+++ hooks.bash
@@ -1,4 +1,11 @@
+#!/usr/bin/env bash
+
+set -eEfuo pipefail
+IFS=$'\n\t'
+shopt -s inherit_errexit
+
 (
+  set +f
   shopt -s globstar nullglob
 
   shellcheck --enable all -- certbot-ocsp-fetcher ./**/*.{ba,}sh
