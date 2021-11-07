8562d607b1241b22c58da3c893e4997c0212e8e2
Tom Wassenberg <github@tomwassenberg.nl>
2021-11-07 14:40:04 +0100

    Add shellcheckrc file

--- hooks.bash
+++ hooks.bash
@@ -8,11 +8,11 @@ shopt -s inherit_errexit
   set +f
   shopt -s globstar nullglob
 
-  shellcheck --enable all -- certbot-ocsp-fetcher ./**/*.{ba,}sh
+  shellcheck -- certbot-ocsp-fetcher ./**/*.{ba,}sh
 
   # We need to exclude SC2154 because of
   # https://github.com/koalaman/shellcheck/issues/1823
-  shellcheck --enable all --exclude 2154 -- ./**/*.bats
+  shellcheck --exclude 2154 -- ./**/*.bats
 )
 
 shfmt -d -s .
